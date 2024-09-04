d95f7c437131ca28a3279b420edc2a9b8cb5a5b8
mohamed yasser <mohamed135095@eng.psu.edu.eg>
2024-09-04 20:09:30 +0300

    ending register

--- E-Learning.DAL/ServicesExtension/DALServicesExtensions.cs
+++ E-Learning.DAL/ServicesExtension/DALServicesExtensions.cs
@@ -36,7 +36,7 @@ namespace E_Learning.DAL.ServicesExtension
                 loggingBuilder.AddDebug();
             });
             /*------------------------------------------------------------------------*/
-            var connectionString = configuration.GetConnectionString("ConStr");
+            var connectionString = configuration.GetConnectionString("Yasser");
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
             /*------------------------------------------------------------------------*/
             services.AddScoped<IAnswerRepository, AnswerRepository>();
