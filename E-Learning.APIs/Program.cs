18d60014ae522cd61c3685c57e8db41ab5cd4000
mohamed yasser <mohamed135095@eng.psu.edu.eg>
2024-09-06 00:37:43 +0300

    adding mail yasser

--- E-Learning.APIs/Program.cs
+++ E-Learning.APIs/Program.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using E_Learning.BL.Configuration;
 
 namespace E_Learning.APIs
 {
@@ -28,11 +30,19 @@ namespace E_Learning.APIs
                 //options.Filters.Add(new AuthorizeFilter(policy));
 
             });
+
+
+            //for configiring the mail
+            builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
+            //adding the mail service
             builder.Services.AddSwaggerGen();
-            /*------------------------------------------------------------------------*/
-            builder.Services.AddBLServices();
+        
+        /*------------------------------------------------------------------------*/
+        builder.Services.AddBLServices();
             builder.Services.AddDALServices(builder.Configuration);
 
 
