67bb243d46e5036f812914709128bb6bb01cc641
mohamed yasser <mohamed135095@eng.psu.edu.eg>
2024-09-08 04:09:10 +0300

    finil forget password

--- E-Learning.BL/Managers/AccountManager/AccountManager.cs
+++ E-Learning.BL/Managers/AccountManager/AccountManager.cs
@@ -1,6 +1,8 @@
-﻿using E_Learning.BL.DTO.User;
+﻿using E_Learning.BL.DTO.Mail;
+using E_Learning.BL.DTO.User;
 using E_Learning.BL.Enums;
 using E_Learning.BL.Managers.AuthenticationManager;
+using E_Learning.BL.Managers.Mailmanager;
 using E_Learning.DAL.Models;
 using E_Learning.DAL.UnitOfWorkDP;
 using Microsoft.AspNetCore.Identity;
@@ -10,6 +12,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace E_Learning.BL.Managers.AccountManager
 {
@@ -21,15 +24,16 @@ namespace E_Learning.BL.Managers.AccountManager
         private readonly RoleManager<Role> _roleManager;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IJwtManager _jwtManager;
-
+        private readonly IMailManager _mailManager;
         public AccountManager(UserManager<User> userManager, SignInManager<User> signInManager,
-            RoleManager<Role> roleManager, IUnitOfWork unitOfWork, IJwtManager jwtManager)
+            RoleManager<Role> roleManager, IUnitOfWork unitOfWork, IJwtManager jwtManager , IMailManager mailManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
             _unitOfWork = unitOfWork;
             _jwtManager = jwtManager;
+            _mailManager = mailManager;
         }
 
         public async Task<IdentityResult> RegisterUserAsync(RegisterDTO registerDTO)
@@ -154,6 +158,80 @@ namespace E_Learning.BL.Managers.AccountManager
             }
 
             return await GenerateNewJwtTokenAsync(user, tokenModel);
+        }
+
+        public async Task<(bool Success, string Message)> ForgetPasswordAsync(ForgetPasswordDTO forgetPasswordDTO)
+        {
+            var user = await _userManager.FindByNameAsync(forgetPasswordDTO.Email);
+            if (user != null)
+            {
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                string encodedToken = HttpUtility.UrlEncode(token);
+                string link = $"http://localhost:4200/forget-passwordToken?token={encodedToken}&email={user.Email}";
+
+                // HTML template for the email
+                string htmlBody = $@"
+                    <html>
+                    <head>
+                        <style>
+                            .container {{ /* Your email styles here */ }}
+                            .button {{ /* Your button styles here */ }}
+                        </style>
+                    </head>
+                    <body>
+                        <div class='container'>
+                            <h2>Password Reset Request</h2>
+                            <p class='message'>You have requested to reset your password. Click the button below to proceed.</p>
+                            <a href='{link}' class='button'>Reset Password</a>
+                            <p>If you did not request a password reset, please ignore this email.</p>
+                            <div class='footer'>
+                                <p>Thank you,<br>The E-Learning Team</p>
+                            </div>
+                        </div>
+                    </body>
+                    </html>";
+
+                MailData mailData = new MailData
+                {
+                    RecieverMail = user.Email,
+                    EmailSubject = "Password Reset Request",
+                    EmailBody = htmlBody
+                };
+
+                // Use mail service to send email
+                if (_mailManager.SendMail(mailData))
+                {
+                    return (true, "Email has been sent successfully.");
+                }
+                else
+                {
+                    return (false, "Error with sending email.");
+                }
+            }
+
+            return (false, "User not found.");
+        }
+        public async Task<(bool Success, string Message)> ResetPasswordAsync(ForgetPasswordDTO forgetPasswordDTO)
+        {
+            if (forgetPasswordDTO == null || string.IsNullOrWhiteSpace(forgetPasswordDTO.Token))
+            {
+                return (false, "Invalid password reset request.");
+            }
+
+            var user = await _userManager.FindByEmailAsync(forgetPasswordDTO.Email);
+            if (user != null)
+            {
+                var resetPasswordResult = await _userManager.ResetPasswordAsync(user, forgetPasswordDTO.Token, forgetPasswordDTO.Password);
+                if (!resetPasswordResult.Succeeded)
+                {
+                    var errors = string.Join(" | ", resetPasswordResult.Errors.Select(e => e.Description));
+                    return (false, errors);
+                }
+
+                return (true, "Password has been reset successfully.");
+            }
+
+            return (false, "User does not exist.");
         }
     }
 }
