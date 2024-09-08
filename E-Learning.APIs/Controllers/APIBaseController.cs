47fce489333c5bade557119f1c3c367630596897
mohsentayseer <mohsen.taysser10@gmail.com>
2024-09-08 18:08:41 +0300

    test

--- E-Learning.APIs/Controllers/APIBaseController.cs
+++ E-Learning.APIs/Controllers/APIBaseController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Learning.APIs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [AllowAnonymous]
     public class APIBaseController : ControllerBase
     {
     }
